ec712a898e49891c779cd16f8919cab09f9ecee6
Poorvi Agrawal <poorvi.pics17@gmail.com>
2025-09-05 21:37:54 +0530

    Update gst.py

--- gst.py
+++ gst.py
@@ -1,4 +1,4 @@
-x=int(input("categories defined for gst \n\n 1.essentials(milk,eggs,fresh vegetables) \n\n 2. packaged food,transport,household essentials \n\n  3. Mobile Phones,processed foods \n\n 4.  all mostly goods and services , electronics and restaurants \n\n 5. luxury goods,tobacco products, expensive cars\t"))
+x=int(input("categories defined for gst \n\n 1.essentials(milk,eggs,fresh vegetables),healthcare,hopstialsand educational services \n\n 2. packaged food,transport,household essentials ,small vehicles,\n\n  3. Mobile Phones,processed foodS, all mostly goods and services , electronics and restaurants \n\n 4. luxury goods,tobacco products, expensive cars\t"))
 total=0
 cop=float(input("enter the cost of product"))
 if(x==1):
@@ -7,13 +7,10 @@ elif(x==2):
     total=cop+(0.05*cop)
     print('total amount=',total,'/-only')
 elif(x==3):
-    total=cop+(0.12*cop)
-    print('total amount=',total,'/-only')
-elif(x==4):
     total=cop+(0.18*cop)
     print('total amount=',total,'/-only')
-elif(x==5):
-    total=cop+(0.28*cop)
+elif(x==4):
+    total=cop+(0.40*cop)
     print('total amount=',total,'/-only')
 else:
     print('invalid input')
